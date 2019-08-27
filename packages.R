eba3830c6da9b70f1482b13a66974a5e980a191f
John Garza <johnegarza@wustl.edu>
2019-08-27 10:50:49 -0500

    More libraries for greater flexibility

--- packages.R
+++ packages.R
@@ -4,3 +4,6 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite("ensembldb")
 biocLite("EnsDb.Hsapiens.v86")
+biocLite("EnsDb.Hsapiens.v75")
+biocLite("EnsDb.Mmusculus.v79")
+biocLite("EnsDb.Mmusculus.v75")
